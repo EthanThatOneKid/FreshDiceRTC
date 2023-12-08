5d6f61d137f442a48465c23b3d23be47d0662ac5
NDH <nick@ndhrones.com>
2023-12-08 07:19:20 -0500

    Update post.ts

--- routes/api/post.ts
+++ routes/api/post.ts
@@ -1,13 +1,12 @@
 import { HandlerContext } from "$fresh/server.ts";
 
 const DEBUG = true
+const gBC = new BroadcastChannel("game");
 
 export const  handler = async (_req: Request, _ctx: HandlerContext): Promise<Response> => {
     const dataObject = await _req.json();
         if (DEBUG) console.info('Client Posted:', dataObject)
-        const gBC = new BroadcastChannel("game");
         gBC.postMessage(dataObject);
-        gBC.close();
         return new Response("",
         {
             status: 200,
